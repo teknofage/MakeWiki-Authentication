e7676d56b5fb21198da237eff43f79c549310a93
teknofage <teknofage@gmail.com>
2020-02-24 16:57:19 -0800

    tests added

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from wiki.models import Page
 
 # Create your tests here.
+
+class WikiTestCase(TestCase):
+    def test_true_is_true(self):
+        """Tests if True is equal to True. Should always pass. """
+        self.assertEqual(True, True)
+        
+        
+    def test_page_slugify_on_save(self):
+        "Tests the slug generated when saving a Page."""
+        # Author is a required field in our model.
+        # Create a user for this test and save it to the test database.
+        user = User()
+        user.save()
+        
+        #Create and save a new page to the test database.
+        page = Page(title="My Test Page", content="test", author=user)
+        page.save()
+        
+        # Make sure the slug that was generated in Page.save()
+        # matches what we think it should be.
+        self.assertEqual(page.slug, "my-test-page")
+        
+class PageListViewTests(TestCase):
+    def test_multiple_pages(self):
+        #make some teest data to be displayed
+        user = User.objects.create()
+        
+        Page.objects.create(title="My Test Page", content="test", author=user)
+        Page.objects.create(title="Another Test Page", content="test", author=user)
+        
+        #Issue a GET request tot he MakeWiki homepage.
+        #When we make a request, we get a response back.
+        response = self.client.get('/')
+        
+        # Check that the response is 200 OK.
+        self. assertEqual(response.status_code, 200)
+        
+        # Check that the number of pages passed to the template
+        # matches the number of pages we have in the database.
+        
+        responses = response.context['pages']
+        self.assertEqual(len(responses), 2)
+    
+        self.assertQuerysetEqual(responses, ['<Page: My Test Page>', '<Page: Another Test Page>'], ordered=False)
